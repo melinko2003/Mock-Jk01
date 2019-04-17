5b0d478abf7fd262846c25340e7aee93d3ef5643
lcurran <lcurran@godaddy.com>
2019-04-17 13:19:34 -0500

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum install wget java -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
